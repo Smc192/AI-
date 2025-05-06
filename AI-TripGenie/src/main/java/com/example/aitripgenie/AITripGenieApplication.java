37f9859f40602c209cda245948d9150d80c639f8
罗天舒 <100849038+Smc192@users.noreply.github.com>
2025-05-07 00:51:09 +0800

    Add files via upload

---
+++ AI-TripGenie/src/main/java/com/example/aitripgenie/AITripGenieApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitripgenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AITripGenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AITripGenieApplication.class, args);
+    }
+
+}
---
+++ AI-TripGenie/src/test/java/com/example/aitripgenie/YuAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitripgenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AITripGenieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
